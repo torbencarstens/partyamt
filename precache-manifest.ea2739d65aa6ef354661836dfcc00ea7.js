dfc12c7908bab39bedab12e7507ffd273703aad3
Torben Carstens <github@carstens.tech>
2019-11-15 17:29:52 +0100

    Updates

---
+++ precache-manifest.ea2739d65aa6ef354661836dfcc00ea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68e751107fe76759d0e15cc74fa6202",
+    "url": "/partyamt/index.html"
+  },
+  {
+    "revision": "0dbc9cd7549c330a9995",
+    "url": "/partyamt/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "f75b6528b443e12e1ac6",
+    "url": "/partyamt/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "0dbc9cd7549c330a9995",
+    "url": "/partyamt/static/js/2.0f29e571.chunk.js"
+  },
+  {
+    "revision": "f75b6528b443e12e1ac6",
+    "url": "/partyamt/static/js/main.b46ab93d.chunk.js"
+  },
+  {
+    "revision": "ea20bbfc4db27cd667e3",
+    "url": "/partyamt/static/js/runtime-main.bc2be7e2.js"
+  }
+]);
